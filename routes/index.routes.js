8c7fb64f871cff68eef022459832dfd9f6498f41
Kushal J <jkushal208@gmail.com>
2025-07-25 14:56:42 +0530

    setup multer with custom destination and filename change

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,32 @@
 const express=require("express")
 const Router=express.Router()
+const multer=require("multer")
 
+
+
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() 
+    cb(null, file.originalname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 Router.get("/home",(req,res)=>{
   res.render("home")
+})
+
+
+
+Router.post("/upload",upload.single("file"),(req,res)=>{
+  res.json(req.file)
+  
 })
 
 
