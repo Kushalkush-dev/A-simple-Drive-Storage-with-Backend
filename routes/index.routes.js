6e8d17f74b2b97c72c90ebf5b037a0c91c130621
Kushal J <jkushal208@gmail.com>
2025-07-25 20:28:45 +0530

    use memstorage instead of diskstorage in multer and used publc_id to send data to cloudinary

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,15 +7,18 @@ const cloudinary=require("../config/cloudinary")
 
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now()    
-    cb(null, uniqueSuffix + '-' +file.originalname )
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/')
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now()    
+//     cb(null, uniqueSuffix + '-' +file.originalname )
+//   }
+// })
+
+
+const storage=multer.memoryStorage();
 
 const upload = multer({ storage: storage })
 
@@ -27,16 +30,22 @@ Router.get("/home",(req,res)=>{
 
 
 Router.post("/upload",upload.single("file"),async (req,res)=>{
- const result=await cloudinary.uploader.upload(req.file.path,
+  const base64=req.file.buffer.toString("base64")
+  const dataUri=`data:${req.file.mimetype};base64,${base64}`
+
+ const result=await cloudinary.uploader.upload(dataUri,
   {
     folder:"sample",
-    use_filename:true
+    public_id:+ Date.now()+"_"+ req.file.originalname ,
+    resource_type:"auto"
   }
  )
  const downloadfile=result.secure_url.replace('/upload',"/upload/fl_attachment")
   res.send(`Upload successfull 
     <img src="${result.secure_url}" width="300" height="200">
     <a href="${downloadfile}" download target="_blank">download</a>`)
+    console.log(req.file);
+    
   
 })
 
