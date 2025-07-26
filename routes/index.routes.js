c26fa2a67161d842213d0dd20f7773f6b091291e
Kushal J <jkushal208@gmail.com>
2025-07-26 17:59:28 +0530

    render the files from database and render it to frontend via ejs and added download functionality

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,8 @@ const express=require("express")
 const Router=express.Router()
 const multer=require("multer")
 const cloudinary=require("../config/cloudinary")
+const Filemodel=require("../models/file.model")
+const authmiddleware=require("../middlewares/auth.middleware")
 
 
 
@@ -22,16 +24,32 @@ const storage=multer.memoryStorage();
 
 const upload = multer({ storage: storage })
 
-Router.get("/home",(req,res)=>{
-  res.render("home")
+Router.get("/home",authmiddleware,async (req,res)=>{
+   
+  const userfiles=await Filemodel.find({
+    user:req.user._id
+  })
+  console.log(userfiles);
+  
+  res.render("home",{
+    files:userfiles
+  }
+  )
 })
 
 
 
 
-Router.post("/upload",upload.single("file"),async (req,res)=>{
+
+
+
+
+Router.post("/upload",authmiddleware,upload.single("file"),async (req,res)=>{
+  
   const base64=req.file.buffer.toString("base64")
   const dataUri=`data:${req.file.mimetype};base64,${base64}`
+
+  
 
  const result=await cloudinary.uploader.upload(dataUri,
   {
@@ -40,15 +58,23 @@ Router.post("/upload",upload.single("file"),async (req,res)=>{
     resource_type:"auto"
   }
  )
- const downloadfile=result.secure_url.replace('/upload',"/upload/fl_attachment")
-  res.send(`Upload successfull 
-    <img src="${result.secure_url}" width="300" height="200">
-    <iframe src="${result.secure_url}" width="50%" height="600px"></iframe>
+
+const downloadfile=result.secure_url.replace('/upload',"/upload/fl_attachment")
+const newfile=await Filemodel.create({
+  path:downloadfile,
+  originalname:req.file.originalname,
+  user:req.user._id
+})
+
+res.redirect("home")
 
 
-   
-    <a href="${downloadfile}" download target="_blank">download</a>`)
-    console.log(req.file);
+
+  // res.send(`Upload successfull 
+  //   <img src="${result.secure_url}" width="300" height="200">
+  //   <embed src="${result.secure_url}" type="application/pdf" width="100%" height= "500">
+  //   <a href="${downloadfile}" download target="_blank">download</a>`)
+  //   console.log(req.file);
     
   
 })
