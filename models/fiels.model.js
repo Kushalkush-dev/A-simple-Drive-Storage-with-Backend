c64e53b477b7d0560d0c11cb544de3ecd4eb73a4
Kushal J <jkushal208@gmail.com>
2025-07-26 15:39:51 +0530

    add file schema

---
+++ models/fiels.model.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose")
+
+const fileSchema=mongoose.Schema({
+  path:{
+    type:String,
+    required:[true,"patth is required"]
+  },
+  originalname:{
+     type:String,
+     required:[true,"patth is required"]
+  },
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:[true,"User is required"]
+  }
+
+})
+
+const Filemodel=mongoose.model("Filemodel",fileSchema)
+
+module.exports=Filemodel;
